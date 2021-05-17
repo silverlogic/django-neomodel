715f736876ac25405747660eb576007d541b73b0
Alisson Patricio <eu@alisson.net>
2021-05-17 14:33:09 -0300

    wip

--- django_neomodel/fields.py
+++ django_neomodel/fields.py
@@ -2,4 +2,11 @@ from neomodel import properties
 
 class UniqueIdProperty(properties.UniqueIdProperty):
     auto_created = False
-    attname = "id"
+    attname = "pk"
+    name = "pk"
+
+
+    # start a neomodel branch to fix this stuff
+    
+    def to_python(self, object_id):
+        return str(object_id)
